ccce033c7b05aef9846249b158d6c3bf9f315d4e
dpekata <dpekata@stevens.edu>
2021-12-02 16:52:34 -0500

    Update UserStory01.py

--- UserStory01.py
+++ UserStory01.py
@@ -1,69 +1,7 @@
 # User story 01, authored by Daniel Pekata
+from CS555Project03 import GED_to_day, GED_to_month, GED_to_year
 from datetime import date
 import unittest
-from CS555Project03 import database
-
-# formats the day for comparison
-def GED_to_day(day):
-    if day == "1":
-        return "01"
-    elif day == "2":
-        return "02"
-    elif day == "3":
-        return "03"
-    elif day == "4":
-        return "04"
-    elif day == "5":
-        return "05"
-    elif day == "6":
-        return "06"
-    elif day == "7":
-        return "07"
-    elif day == "8":
-        return "08"
-    elif day == "9":
-        return "09"
-    else: return day
-
-# formats the month for comparison
-def GED_to_month(month):
-    if month == "JAN":
-        return "01"
-    elif month == "FEB":
-        return "02"
-    elif month == "MAR":
-        return "03"
-    elif month == "APR":
-        return "04"
-    elif month == "MAY":
-        return "05"
-    elif month == "JUN":
-        return "06"
-    elif month == "JUL":
-        return "07"
-    elif month == "AUG":
-        return "08"
-    elif month == "SEP":
-        return "09"
-    elif month == "OCT":
-        return "10"
-    elif month == "NOV":
-        return "11"
-    else: return "12"
-
-def GED_to_year(year):
-    strlen = len(year)
-    if strlen == 1:
-        return "000" + year
-    elif strlen == 2:
-        return "00" + year
-    elif strlen == 3:
-        return "0" + year
-    elif strlen == 4:
-        return year
-    # if the year is 5 or more digits, it takes place in the future.
-    # so for simplicity, say the year is 9999, and fail the cases that way.
-    else: return "9999"
 
 # checks if a given date has occured before the present day
 def GED_to_date(ged_date):
@@ -97,8 +35,8 @@ def GED_before_date(ged_date):
     if not isinstance(ged_date, str):
         return False
     # remove the starting part of the ged date, namely the "2 DATE " part
-    # format_date = ged_date[7:]
-    result = GED_to_date(ged_date)
+    format_date = ged_date[7:]
+    result = GED_to_date(format_date)
     today = str(date.today())
     # compare today's year with the given year
     while True:
@@ -139,35 +77,28 @@ def GED_before_date(ged_date):
         return False
     else: return True
 
-individuals, families = database()
-for individual in individuals:
-    GED_before_date(individual[3])
-    GED_before_date(individual[6])
-for family in families:
-    GED_before_date(family[1])
-    GED_before_date(family[2])
 
 class TestStringMethods(unittest.TestCase):
     # Testing for US01
     # test that a GED date of may 16th, 1923 is valid
     def test1(self):
-        self.assertEqual(GED_before_date("16 MAY 1923"), True)
+        self.assertEqual(GED_before_date("2 DATE 16 MAY 1923"), True)
     
     # test that a GED date of may 16th, 2023 is not valid
     def test2(self):
-        self.assertEqual(GED_before_date("16 MAY 2023"), False)
+        self.assertEqual(GED_before_date("2 DATE 16 MAY 2023"), False)
 
     # test that a GED date of sep 25th, 2021 is valid (this is the current day as of testing)
     def test3(self):
-        self.assertEqual(GED_before_date("25 SEP 2021"), True)
+        self.assertEqual(GED_before_date("2 DATE 25 SEP 2021"), True)
     
     #test that a GED date of dec 31st, 2021 is not valid 
     def test4(self):
-        self.assertEqual(GED_before_date("31 DEC 2021"), False)
+        self.assertEqual(GED_before_date("2 DATE 31 DEC 2021"), False)
     
     # test that a GED date of july 1st, 100 is valid
     def test5(self):
-        self.assertEqual(GED_before_date("1 JUL 100"), True)
+        self.assertEqual(GED_before_date("2 DATE 1 JUL 100"), True)
     # test that a nondate input is considered false
     def test6(self):
         self.assertEqual(GED_before_date(1), False)
